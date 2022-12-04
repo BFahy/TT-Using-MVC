d81c2cde841ce2d0a82ce006332781071d7aaf91
BFahy <40779493+BFahy@users.noreply.github.com>
2022-12-04 14:40:35 +1000

    Add files via upload

---
+++ Services/EncryptionService.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Cryptography;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace DataAccess.Services
+{
+    public class EncryptionService
+    {
+        string secretKey;
+
+        public EncryptionService(IConfiguration config)
+        {
+            secretKey = config["SecretKey"];
+        }
+
+        /**
+         * Encryption method for file (requires file in byte array format)
+         */
+        public byte[] EncryptByteArray(byte[] fileData)
+        {
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(secretKey);
+
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor();
+
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    msEncrypt.Write(aesAlg.IV, 0, 16);
+
+                    using(CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csEncrypt.Write(fileData, 0 ,fileData.Length);
+                        csEncrypt.FlushFinalBlock();
+                        return msEncrypt.ToArray();
+                    }
+                }
+            }
+        }
+
+        /**
+         * Decryption method for file (requires file in byte array/encrypted format)
+         */
+        public byte[] DecryptByteArray(byte[] encryptedData)
+        {
+            using (AesManaged aesAlg = new AesManaged())
+            {
+                aesAlg.Key = System.Text.Encoding.UTF8.GetBytes(secretKey);
+
+                byte[] IV = new byte[16];
+                Array.Copy(encryptedData, IV, 16);
+                aesAlg.IV = IV;
+
+                ICryptoTransform encryptor = aesAlg.CreateDecryptor();
+
+                using (MemoryStream msDecrypt = new MemoryStream())
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        csDecrypt.Write(encryptedData, 16, encryptedData.Length - 16);
+                        csDecrypt.FlushFinalBlock();
+                        return msDecrypt.ToArray();
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Services/FileLoaderService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using System.IO;
+
+namespace DataAccess.Services
+{
+    public class FileLoaderService
+    {
+        string uploadPath;
+
+        EncryptionService encryptionService;
+
+        public FileLoaderService(IWebHostEnvironment env, EncryptionService encryption)
+        {
+            uploadPath = Path.Combine(env.WebRootPath, "Manuals");
+            encryptionService = encryption;
+        }
+        
+        /*
+         * Using memory stream and filestream to write file
+         * Requires IFormFile (file) as parameter
+         */
+        public async Task SaveFile(IFormFile file)
+        {
+            byte[] fileContents;
+
+            using (MemoryStream stream = new MemoryStream())
+            {
+                await file.CopyToAsync(stream);
+
+                fileContents = stream.ToArray();
+            }
+
+            byte[] encryptedContents = encryptionService.EncryptByteArray(fileContents); 
+            using (MemoryStream dataStream = new MemoryStream(encryptedContents))
+            {
+                string targetFile = Path.Combine(uploadPath, file.FileName);
+
+                using (FileStream fileStream = new FileStream(targetFile, FileMode.Create))
+                {
+                    dataStream.WriteTo(fileStream);
+                }
+            }
+        }
+
+        /**
+         * Loading file from directory, requires filename as parameter
+         */
+        public async Task<FileInfo> LoadFile(string fileName)
+        {
+            DirectoryInfo directory = new DirectoryInfo(uploadPath);
+            if (directory.EnumerateFiles().Any(c=>c.Name.Equals(fileName, StringComparison.OrdinalIgnoreCase)) == false)
+            {
+                return null;
+            }
+
+            return directory.EnumerateFiles().Where(c => c.Name.Equals(fileName, StringComparison.OrdinalIgnoreCase)).FirstOrDefault();
+        }
+
+        /**
+         * Returning byte array containing file information
+         */
+        public async Task<byte[]> ReadFileIntoMemory(string fileName)
+        {
+            var file = await LoadFile(fileName);
+
+            if (file==null)
+            {
+                return null;
+            }
+
+            using (var memStream = new MemoryStream())
+            {
+                using (var fileStream = File.OpenRead(file.FullName))
+                {
+                    fileStream.CopyTo(memStream);
+                    return memStream.ToArray();
+                }
+            }
+        }
+
+        /**
+         * Loading encrypted file as byte array
+         */
+        public async Task<byte[]> LoadEncryptedFile(string fileName)
+        {
+            var encryptedFile = await ReadFileIntoMemory(fileName);
+
+            if (encryptedFile == null || encryptedFile.Length == 0)
+            {
+                return null;
+            }
+
+            var decryptedData = encryptionService.DecryptByteArray(encryptedFile);
+
+            return decryptedData;
+        }
+    }
+}
