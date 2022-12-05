1b4d5fabdeab6826a782c89e84f88c51db6c4f3a
BFahy <40779493+BFahy@users.noreply.github.com>
2022-12-05 18:24:51 +1000

    Add files via upload

---
+++ Startup.cs
@@ -0,0 +1,86 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DataAccess;
+using DataAccess.Services;
+using Microsoft.AspNetCore.Authentication.Cookies;
+
+namespace TypicalTools
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSession(c =>
+            {
+                c.IdleTimeout = TimeSpan.FromSeconds(300);
+                c.Cookie.HttpOnly = true;
+                c.Cookie.IsEssential = true;
+            });
+            services.AddDistributedMemoryCache();
+
+            services.AddSingleton<DBContext>();
+
+            services.AddControllersWithViews();
+
+            services.AddScoped<FileLoaderService>();
+            services.AddScoped<EncryptionService>();
+
+            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie(options =>
+            {
+                options.LoginPath = "/Admin/Login";
+                options.AccessDeniedPath = "/Admin/AccessDenied";
+                options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+                options.SlidingExpiration = true;
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+
+            app.UseRouting();
+
+            app.UseSession();
+
+            app.UseAuthentication();
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+            });
+        }
+    }
+}
